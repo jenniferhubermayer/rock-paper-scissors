94e66f855b6f8e4da4663140a20c69c0a4ccaafa
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-07-22 11:17:02 +0200

    Clean Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,21 @@ let computerHandTypesArray = ["scissors", "stone", "paper"];
 let counterOutputUser = document.querySelector("#counter-output-user");
 let counterOutputComputer = document.querySelector("#counter-output-computer");
 let roundTextWinner = document.querySelector("#round-text-winner");
+let sectionRounds = document.querySelector(".rounds");
+let sectionCounter = document.querySelector(".counter");
+let sectionPlay = document.querySelector(".play");
+let sectionRoundWinner = document.querySelector("#round-text-winner");
+let sectionEndWinner = document.querySelector("#end-text-winner");
+let sectionFooter = document.querySelector("footer");
 
 // ---------------
 // STYLE
 // ---------------
 
-document.querySelector(".counter").style.display = "none";
-document.querySelector(".play").style.display = "none"
+sectionCounter.style.display = "none";
+sectionPlay.style.display = "none";
+sectionRoundWinner.style.display = "none";
+sectionEndWinner.style.display = "none";
 
 // ---------------
 // VALUES
@@ -60,9 +68,9 @@ countRounds.forEach((btn) => {
 document.querySelector("#submit").addEventListener("click", (event) => {
   event.preventDefault();
   if (roundsNumberSelectedOutput.innerText >= 5){
-  document.querySelector(".rounds").style.display = "none";
-  document.querySelector(".counter").style.display = "flex";
-  document.querySelector(".play").style.display = "flex"
+  sectionRounds.style.display = "none";
+  sectionCounter.style.display = "flex";
+  sectionPlay.style.display = "unset"
   }
   else{
     window.alert("First things first. Please choose the number of rounds you wish to play!")
@@ -85,52 +93,62 @@ userHandButton.forEach((btn) => {
     let computerHandTypesIndex = Math.floor(Math.random() * 3);
     let computersHand = computerHandTypesArray[computerHandTypesIndex];
 
-    // Entfernt die CSS Klasse für das Händeschütteln und die Buttons:
+    // Entfernt die CSS Klasse für das Händeschütteln, die Buttons und Gewinnertexte:
     removeShakeHands();
-    document.querySelector(".play").style.display = "none"
+    sectionPlay.style.display = "none"
+    sectionRoundWinner.style.display = "none";
+    sectionEndWinner.style.display = "none";
 
     // Fügt die CSS Klasse nach einem Timeout hinzu und gibt die Hand des Users aus.
     setTimeout(function(){
       let usersHand = event.target.parentNode.id;
       roundTextWinner.innerText = "";
       addShakeHands();
+      sectionFooter.style.display = "none";
 
       // Checke den Gewinner der Runde, nach 3200 Millisekunden:
       setTimeout(function checkRoundWinner(){
         if (usersHand === computersHand) {
           roundTextWinner.innerHTML = `<span>${usersHand}</span> against <span>${computersHand}</span>.<br>Oops! This round was a draw.`;
-          document.querySelector(".play").style.display = "flex"
+          sectionRoundWinner.style.display = "unset";
+          sectionPlay.style.display = "unset";
+          sectionFooter.style.display = "inherit";
         }
         else if (usersHand == "paper" && computersHand == "stone" || usersHand == "scissors" && computersHand == "paper" || usersHand == "stone" && computersHand == "scissors" ){
           roundTextWinner.innerHTML = `<span>${usersHand}</span> beats <span>${computersHand}</span>.<br>One point for you!`;
-          document.querySelector(".play").style.display = "flex"
+          sectionRoundWinner.style.display = "unset";
+          sectionPlay.style.display = "unset";
+          sectionFooter.style.display = "inherit";
           counterRankUser ++;
           counterOutputUser.innerText = counterRankUser;
         }
         else if (computersHand == "paper" && usersHand == "stone" || computersHand == "scissors" && usersHand == "paper" || computersHand == "stone" && usersHand == "scissors" ){
           roundTextWinner.innerHTML = `<span>${computersHand}</span> beats <span>${usersHand}</span>.<br>One point for the computer!`;
-          document.querySelector(".play").style.display = "flex"
+          sectionRoundWinner.style.display = "unset";
+          sectionPlay.style.display = "unset";
+          sectionFooter.style.display = "inherit";
           counterRankComputer ++;
           counterOutputComputer.innerText = counterRankComputer;
         }
-      }, 3200)
+      }, 2500)
 
       // Beende das Spiel, wenn die Runden gespielt sind:
       setTimeout(function checkRoundCount(){
         if (roundsNumberRankOutput.innerText == roundsNumberSelectedOutput.innerText){
-          console.log("Alle Runden sind gespielt");
-          document.querySelector(".play").style.display = "none";
+          sectionPlay.style.display = "none";
+          sectionEndWinner.style.display = "unset";
+          sectionFooter.style.display = "inherit";
           if (counterRankUser > counterRankComputer){
-            document.querySelector("#end-text-winner").innerHTML = "Woohoo! You won!"
+            sectionEndWinner.innerHTML = "Woohoo! You won!"
           }
           else if (counterRankUser < counterRankComputer){
-            document.querySelector("#end-text-winner").innerHTML = "You're a looser! Try it again!"
+            sectionEndWinner.innerHTML = "You're a looooooser! Try it again!"
           }
           else{
-            document.querySelector("#end-text-winner").innerHTML = "Looks like this game was draw. Try it again!"
+            sectionEndWinner.innerHTML = "Looks like this game was a draw. Try it again!"
           }
         }
-      }, 3200)
+      }, 2500)
     })
   });
 });
